875bec975a748579a5f727ac59feac762607178c
Stefano <stefano.zaccaria29@gmail.com>
2020-06-23 16:21:04 +0200

    ex complete

--- mail/script.js
+++ mail/script.js
@@ -3,6 +3,7 @@ var userMail = prompt("inserisci la tua mail:");
 var mail = ["stefano@hotmail.com", "roberto@hotmail.com", "alessandro@hotmail.com", "francesco@hotmail.com", "massimo@hotmail.com", "angela@hotmail.com",];
 
 var msg = document.getElementById('message');
+var j = 0;
 
 // if (mail.indexOf(userMail) === -1) {
 //    document.getElementById('message').innerHTML = "this mail doesn't exist"
@@ -14,9 +15,10 @@ for (var i = 0; i < mail.length; i++) {
    if (mail[i] == userMail ) {
       msg.innerHTML = "WELCOME"
       console.log(userMail);
-      break;
    } else {
-      msg.innerHTML = "this mail doesn't exist"
-      console.log(userMail);
+      j++;
+   }
+   if (j == mail.length) {
+      msg.innerHTML = "la mail non esiste"
    }
 };
