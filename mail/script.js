a80c215eea1c355697c5268c19761c2f85537e84
Stefano <stefano.zaccaria29@gmail.com>
2020-06-23 15:28:56 +0200

    ex con for

--- mail/script.js
+++ mail/script.js
@@ -2,8 +2,18 @@ var userMail = prompt("inserisci la tua mail:");
 
 var mail = ["stefano@hotmail.com", "roberto@hotmail.com", "alessandro@hotmail.com", "francesco@hotmail.com", "massimo@hotmail.com", "angela@hotmail.com",];
 
-if (mail.indexOf(userMail) === -1) {
-   document.getElementById('message').innerHTML = "this mail doesn't exist"
-} else {
-   document.getElementById('message').innerHTML = "WELCOME"
+var msg = document.getElementById('message');
+
+// if (mail.indexOf(userMail) === -1) {
+//    document.getElementById('message').innerHTML = "this mail doesn't exist"
+// } else {
+//    document.getElementById('message').innerHTML = "WELCOME"
+// };
+
+for (var i = 0; i < mail.length; i++) {
+   if (mail[i] == userMail) {
+      msg.innerHTML = "WELCOME"
+   } else {
+      msg.innerHTML = "this mail doesn't exist"
+   }
 };
